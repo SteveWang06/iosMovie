389602ee35b77f334b639cb8f7a13a2abe42ca1e
SteveWang06 <thanhphuoc1306vn@gmail.com>
2025-07-08 13:51:44 +0800

    login

---
+++ iosmovie/Shared/Views/SplashView.swift
@@ -0,0 +1,18 @@
+//
+//  SplashView.swift
+//  iosmovie
+//
+//  Created by Nguyen Thanh phuoc on 2025/7/6.
+//
+
+import SwiftUI
+
+struct SplashView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SplashView()
+}
