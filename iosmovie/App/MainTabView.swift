ec7c28fbaa2c07b8f20490eb93297eb395705b1e
SteveWang06 <thanhphuoc1306vn@gmail.com>
2025-07-08 18:30:01 +0800

    feat: demo TabSection

--- iosmovie/App/MainTabView.swift
+++ iosmovie/App/MainTabView.swift
@@ -9,9 +9,28 @@ import SwiftUI
 
 struct MainTabView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        TabView {
+            MovieView()
+                .tabItem {
+                    Image(systemName: "film")
+                    Text("Movie")
+                }
+
+            TVShowView()
+                .tabItem {
+                    Image(systemName: "tv")
+                    Text("TV Show")
+                }
+
+            AccountView()
+                .tabItem {
+                    Image(systemName: "person.crop.circle")
+                    Text("Account")
+                }
+        }
     }
 }
+
 
 #Preview {
     MainTabView()
