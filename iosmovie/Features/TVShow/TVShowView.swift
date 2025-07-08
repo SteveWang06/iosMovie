1ad9fc721884b5c26f74f91c53441104c485307f
SteveWang06 <thanhphuoc1306vn@gmail.com>
2025-07-08 18:30:01 +0800

    feat: demo TabSection

--- iosmovie/Features/TVShow/TVShowView.swift
+++ iosmovie/Features/TVShow/TVShowView.swift
@@ -7,11 +7,17 @@
 
 import SwiftUI
 
+import SwiftUI
+
 struct TVShowView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            Text("TV Show View")
+                .navigationTitle("TV Shows")
+        }
     }
 }
+
 
 #Preview {
     TVShowView()
