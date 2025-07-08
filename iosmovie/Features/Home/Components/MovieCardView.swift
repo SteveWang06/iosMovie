5b99f215c03c58c79da6df669d2671500aaa0b4c
SteveWang06 <thanhphuoc1306vn@gmail.com>
2025-07-08 13:51:44 +0800

    login

---
+++ iosmovie/Features/Home/Components/MovieCardView.swift
@@ -0,0 +1,18 @@
+//
+//  MovieCardView.swift
+//  iosmovie
+//
+//  Created by Nguyen Thanh phuoc on 2025/7/6.
+//
+
+import SwiftUI
+
+struct MovieCardView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MovieCardView()
+}
---
+++ iosmovie/Features/Home/HomeView.swift
@@ -0,0 +1,18 @@
+//
+//  HomeView.swift
+//  iosmovie
+//
+//  Created by Nguyen Thanh phuoc on 2025/7/6.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HomeView()
+}
