449c28b081d5931fb031b0b0666ccd55fd58f2cd
SteveWang06 <thanhphuoc1306vn@gmail.com>
2025-07-09 17:03:54 +0800

    feat: movie content

---
+++ iosmovie/Features/Movie/Components/SelectedGenreView.swift
@@ -0,0 +1,18 @@
+//
+//  SelectedGenreView.swift
+//  iosmovie
+//
+//  Created by Nguyen Thanh phuoc on 2025/7/9.
+//
+
+import SwiftUI
+
+struct SelectedGenreView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SelectedGenreView()
+}
---
+++ iosmovie/Features/Movie/MovieListView.swift
@@ -0,0 +1,18 @@
+//
+//  MovieListView.swift
+//  iosmovie
+//
+//  Created by Nguyen Thanh phuoc on 2025/7/9.
+//
+
+import SwiftUI
+
+struct MovieListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MovieListView()
+}
